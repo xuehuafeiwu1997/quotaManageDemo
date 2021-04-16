634926322f22a9407ad8bd9dbe3d8ec9f9e0c691
xumingyang <xumingyang@greatyun.com>
2021-04-16 13:12:37 +0800

    初始搭建

---
+++ src/test/java/com/xmy/quotamanage/QuotaManageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xmy.quotamanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuotaManageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
